d075b74bb35ea7a33efe92b337dc7c58c1547b03
Jase2z <ighawvb@gmail.com>
2023-07-21 14:52:35 -0700

    a

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ import algebrite from "https://cdn.skypack.dev/algebrite@1.4.0"
 function get_difficulty(mod_skill) {
   // from rollGaussian...slide = ((pow(modSkill, 3) - pow(difficulty, 3)) / 50000 + (modSkill - difficulty))
   // algebreticly altered too, where slide is 20:
-  //-diff^3 -50,000diff = -mod_skill^3 -50,000mod_skill -1mill
+  //-diff^3 -50,000diff = -mod_skill^3 -50,000mod_skill + 1mill
   // Option 1 is to get a cubic equation solver library. Goodness, needle in a haystack -- http://algebrite.org/, https://www.npmjs.com/package/algebrite
   // Option 2, I know diff is 0 to 100 and can estimate at 5 step granularity. Maybe do simulations in steps of 5 for diff looking for whichever is closests to equaling 0.
   
@@ -35,7 +35,7 @@ function get_difficulty(mod_skill) {
 
 function get_farm_group(difficulty) {
   var toReturn;
-  if (difficulty >= 0 && difficulty < 5.5) {
+  if ( difficulty < 5.5) {
     // potato at 4 diff.
     toReturn = {
       easy: "nothing at 0 difficulty.",
@@ -158,41 +158,45 @@ function get_farm_group(difficulty) {
  * Find effective skill using skill, tool ql and bonus.
  * @param {number} skill - the character's skill.
  * @param {number} tool_ql - the quality of tool used.
- * @param {number} bonus - A buff from tool skill and parent skill when applicable.
+ * @param {number} bonus - A buff skill.
  * @returns {number}
  */
 function effective_skill(skill, tool_ql, bonus=0){
-  var skill = Number(skill);
-  var tool_ql = Number(tool_ql);
-  var bonus = Number(bonus);
-  if (bonus > 70){
-    bonus = 70;
-  }
-  var eff, linear_max;
+  skill = Number(skill);
+  let eff, linear_max;
+  tool_ql = Number(tool_ql);  
+
   if (tool_ql < skill) {
     // A tool can reduce skill by equal weighting.
     eff = (skill + tool_ql) / 2;
   } else {
-    // A tool shouldn't increase skill by equal weighting. Limit to < 100% increase on skill 
-    // and have the amount by which tool ql exceds skill be the % increase.
+    // A tool shouldn't increase skill by equal weighting. Limit to < 100% 
+    // increase on skill and have the amount by which tool ql exceds skill be 
+    // the % increase.
     // linear_max is the % of skill increase.
     linear_max = tool_ql - skill;
     eff = skill + skill * linear_max / 100;
   }
-  
-    // 1. bonus is a % of effective_Skill up to about 33 effective_Skill. Then it's slowly(linear) phased out
-    //    and 100 skill it is 0. Bonus is multipled by diffToMaxChange after about 33 effective_Skill
-    //    and eventually it hits 0 giving no bonus.
-    //2. I need diffToMaxChange value as an estimator to increase simplafied difficulty (effective skill - 20).
-    linear_max = (100 + eff) / 2;
-    var diffToMaxChange = Math.min(eff, linear_max - eff);
+
+  bonus = Number(bonus);
+  if (bonus > 70){
+    bonus = 70;
+  }
+  // 1. bonus is a % of effective_Skill up to about 33 effective_Skill. Then
+  //    it's slowly(linear) phased out and 100 skill it is 0. Bonus is 
+  //    multipled by diffToMaxChange after about 33 effective_Skill and
+  //    eventually it hits 0 giving no bonus.
+  // 2. I need diffToMaxChange value as an estimator to increase simplafied 
+  //    difficulty (effective skill - 20).
+  linear_max = (100 + eff) / 2;
+  var diffToMaxChange = Math.min(eff, linear_max - eff);
   
   if (bonus > 0) {
     var new_bon = diffToMaxChange * bonus / 100;
     eff += new_bon;
   }
 
-  return {eff:eff, diff_estimator:diffToMaxChange};
+  return eff;
 }
 
 /**
@@ -203,28 +207,63 @@ function effective_skill(skill, tool_ql, bonus=0){
 function get_gaussian_mean(diff, eff) {
   var diff = Number(diff);
   var eff = Number(eff);
-  // from rollGaussian...slide = ((pow(modSkill, 3) - pow(difficulty, 3)) / 50000 + (modSkill - difficulty))
+  //  from rollGaussian...slide = ((pow(modSkill, 3) - pow(difficulty, 3))
+  //    / 50000 + (modSkill - difficulty))
   return ((Math.pow(eff, 3) - Math.pow(diff, 3)) / 50000 + (eff - diff))
 }
 
 /**
- *  Farming skill related solver.
+ *  Get crop that best matches difficulty and skill. Optimal difficulty is when
+ *  gaussian mean is 20.
  * @param {number} skill 
- * @param {number} difficulty 
  * @param {number} rake_ql 
  * @param {number} rake_skill 
  * @param {number} nature_skill 
  * @returns {object[]} 
  */
-function farming(skill, difficulty, rake_ql, rake_skill, nature_skill) {
+function farming_get_diff(skill, rake_ql, rake_skill, nature_skill) {
+  let f_skills = [];
+  let f_skill = {order:1000, bonus:0, eff:0, mean:0, diff:0}
+  f_skills.push(f_skill)
+  let _eff = effective_skill(skill, rake_ql, 0);
+  let _bonus; let _mean; let _order; let index; let for_end;
+  if (_eff - 30 < 0) {
+    index = 0
+    for_end = _eff
+  } else {
+    index = _eff - 30
+    for_end = _eff
+  }
+  for (; index < for_end; index++) {
+    _bonus = get_gaussian_mean(index, nature_skill) / 10;
+    _bonus += get_gaussian_mean(index, rake_skill);
+    _bonus = Math.min(70, _bonus);
+    _bonus = Math.max(0, _bonus);
+    _eff = effective_skill(skill, rake_ql, _bonus);
+    _mean = get_gaussian_mean(index, _eff);
+    _order = Math.abs(_mean - 20)
+    f_skill = {order:_order, bonus:_bonus, eff:_eff, mean:_mean, diff:index}
+    f_skills.push(f_skill)
+  }
+  f_skills.sort(function(a, b){return a.order - b.order})
+  const x = f_skills[0]
+  console.log(f_skills)
+  console.log("bonus:" + x.bonus.toFixed(2) + " mean:" + x.mean.toFixed(2) 
+    + " eff:" + x.eff.toFixed(2) + " diff:" + x.diff)
+  
+  return {eff1:x.eff, diff1:x.diff, bonus:x.bonus}
+}
+
+function farmingOld(skill, difficulty, rake_ql, rake_skill, nature_skill) {
   if(skill === undefined) {
 
   }
   if (difficulty === undefined) {
     nature_skill = nature_skill || Math.round(skill * 0.50);
-    // To calculate difficulty bonus is needed, but to get bonus difficulty is needed! 
+    // To calculate difficulty bonus is needed, but to get bonus difficulty 
+    // is needed! 
     // Use effective_skill - 20 equals difficulty for bonus modifier related.
-    //eff:eff, diff_estimator:diffToMaxChange
+    // eff:eff, diff_estimator:diffToMaxChange
     var eff_skill = effective_skill(skill, rake_ql);
     var eff_no_b = eff_skill.eff
     var eff2 = eff_no_b
@@ -252,49 +291,111 @@ function farming(skill, difficulty, rake_ql, rake_skill, nature_skill) {
 }
 
 
-document.getElementById("skill_span").innerHTML = document.getElementById(
-  "skill_in"
-).value;
-document.getElementById("tool_ql_span").innerHTML = document.getElementById(
-  "tool_ql_in"
-).value;
-document.getElementById("tool_skill_span").innerHTML = document.getElementById(
-  "tool_skill_in"
-).value;
-var farm_result = farming(document.getElementById("skill_in").value, undefined, 
+const skill_label_default = "Farming skill: ... "
+const skill_input = document.getElementById("skill_in");
+const skill_label = document.getElementById("skill_in_label");
+skill_label.innerHTML = skill_label_default + skill_input.value;
+
+const tool_ql_default = "Tool QL: ........... "
+const tool_ql_input = document.getElementById("tool_ql_in");
+const tool_ql_label = document.getElementById("tool_ql_label");
+tool_ql_label.innerHTML = tool_ql_default + tool_ql_input.value;
+
+const tool_skill_default = "Tool skill: ......... "
+const tool_skill_input = document.getElementById("tool_skill_in");
+const tool_skill_label = document.getElementById("tool_skill_label");
+tool_skill_label.innerHTML = tool_skill_default + tool_skill_input.value;
+
+const parent_skill_default = "Parent skill: ...... "
+const parent_skill_input = document.getElementById("parent_skill_in");
+const parent_skill_label = document.getElementById("parent_skill_label");
+parent_skill_label.innerHTML = parent_skill_default + parent_skill_input.value;
+
+var farm_result = farming_get_diff(document.getElementById("skill_in").value, 
                         document.getElementById("tool_ql_in").value,
-                        document.getElementById("tool_skill_in").value);
-document.getElementById("Modified_span").innerHTML = String(farm_result.eff1.toFixed(1)).padStart(4, "0");
-document.getElementById("diff_span").innerHTML = String(farm_result.diff1.toFixed(1)).padStart(4, "0");
+                        document.getElementById("tool_skill_in").value,
+                        document.getElementById("parent_skill_in").value);
+
+document.getElementById("Modified_span").innerHTML = 
+  String(farm_result.eff1.toFixed(1)).padStart(4, "0");
+document.getElementById("diff_span").innerHTML = 
+  String(farm_result.diff1.toFixed(1)).padStart(4, "0");
 get_farm_group(farm_result.diff1);
 
 
-document.getElementById("skill_in").oninput = function () {
-  document.getElementById("skill_span").innerHTML = String(this.value).padStart(2, "0");
-  var farm_result = farming(this.value, undefined, 
+const group_skills = document.getElementById("group_skills");
+
+group_skills.onclick = function () {
+  if (group_skills.checked){
+    group_skills.checked = false;
+  } else {
+    group_skills.checked = true;
+  }
+
+  if (group_skills.hasAttributes()) {
+    let output
+    for (const attr of group_skills.attributes) {
+      output += `${attr.name} -> ${attr.value}\n`;
+    }
+    console.log(output)
+
+  } else {
+    console.log("no attributes")
+  }
+}
+
+skill_input.oninput = function () {
+
+  skill_label.innerHTML = skill_label_default + String(skill_input.value).padStart(2, "0");
+  var farm_result = farming_get_diff(this.value,
                           document.getElementById("tool_ql_in").value,
-                          document.getElementById("tool_skill_in").value);
-  document.getElementById("Modified_span").innerHTML = String(farm_result.eff1.toFixed(1)).padStart(4, "0");
-  document.getElementById("diff_span").innerHTML = String(farm_result.diff1.toFixed(1)).padStart(4, "0");
+                          document.getElementById("tool_skill_in").value,
+                          document.getElementById("parent_skill_in").value);
+  document.getElementById("Modified_span").innerHTML = 
+    String(farm_result.eff1.toFixed(1)).padStart(4, "0");
+  document.getElementById("diff_span").innerHTML = 
+    String(farm_result.diff1.toFixed(1)).padStart(4, "0");
   get_farm_group(farm_result.diff1);
 };
 
-document.getElementById("tool_ql_in").oninput = function () {
-  document.getElementById("tool_ql_span").innerHTML = String(this.value).padStart(2, "0");
-  var farm_result = farming( document.getElementById("skill_in").value, 
-                          undefined, this.value,
-                          document.getElementById("tool_skill_in").value);
-  document.getElementById("Modified_span").innerHTML = String(farm_result.eff1.toFixed(1)).padStart(4,"0");
-  document.getElementById("diff_span").innerHTML = String(farm_result.diff1.toFixed(1)).padStart(4,"0");
+tool_ql_input.oninput = function () {
+  tool_ql_label.innerHTML = tool_ql_default 
+    + String(tool_ql_input.value).padStart(2, "0");
+  var farm_result = farming_get_diff( document.getElementById("skill_in").value,  
+                          this.value,
+                          document.getElementById("tool_skill_in").value,
+                          document.getElementById("parent_skill_in").value);
+  document.getElementById("Modified_span").innerHTML = 
+    String(farm_result.eff1.toFixed(1)).padStart(4,"0");
+  document.getElementById("diff_span").innerHTML = 
+    String(farm_result.diff1.toFixed(1)).padStart(4,"0");
   get_farm_group(farm_result.diff1);
 };
 
-document.getElementById("tool_skill_in").oninput = function () {
-  document.getElementById("tool_skill_span").innerHTML = String(this.value).padStart(2, "0");
-  var farm_result = farming( document.getElementById("skill_in").value, 
-                         undefined, document.getElementById("tool_ql_in").value,
+tool_skill_input.oninput = function () {
+  tool_skill_label.innerHTML = tool_skill_default 
+    + String(tool_skill_input.value).padStart(2, "0");
+  var farm_result = farming_get_diff( document.getElementById("skill_in").value, 
+                         document.getElementById("tool_ql_in").value,
+                         this.value,
+                         document.getElementById("parent_skill_in").value);
+  document.getElementById("Modified_span").innerHTML = 
+    String(farm_result.eff1.toFixed(1)).padStart(4,"0");
+  document.getElementById("diff_span").innerHTML = 
+    String(farm_result.diff1.toFixed(1)).padStart(4,"0");
+  get_farm_group(farm_result.diff1);
+}
+
+parent_skill_input.oninput = function () {
+  parent_skill_label.innerHTML = parent_skill_default 
+    + String(parent_skill_input.value).padStart(2, "0");
+  var farm_result = farming_get_diff( document.getElementById("skill_in").value, 
+                         document.getElementById("tool_ql_in").value,
+                         document.getElementById("tool_skill_in").value,
                          this.value);
-  document.getElementById("Modified_span").innerHTML = String(farm_result.eff1.toFixed(1)).padStart(4,"0");
-  document.getElementById("diff_span").innerHTML = String(farm_result.diff1.toFixed(1)).padStart(4,"0");
+  document.getElementById("Modified_span").innerHTML = 
+    String(farm_result.eff1.toFixed(1)).padStart(4,"0");
+  document.getElementById("diff_span").innerHTML = 
+    String(farm_result.diff1.toFixed(1)).padStart(4,"0");
   get_farm_group(farm_result.diff1);
 }
